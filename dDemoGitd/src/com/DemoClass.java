24e7fe91a07c1688f8c84e083810824ab53eb04c
akshayhiwrale7 <akshay.hiwrale6@gmail.com>
2023-12-20 20:44:28 +0530

    created new class

--- dDemoGitd/src/com/DemoClass.java
+++ dDemoGitd/src/com/DemoClass.java
@@ -1,5 +1,14 @@
 package com;
 
 public class DemoClass {
+private int moNumber;
+
+public int getMoNumber() {
+	return moNumber;
+}
+
+public void setMoNumber(int moNumber) {
+	this.moNumber = moNumber;
+}
 
 }
